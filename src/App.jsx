cc47225ed7dde5dc1529cbc1aa507fc532ce2bc6
Aditya <adityak4347@gmail.com>
2025-06-27 04:55:46 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,14 @@ import CoursesPage from "./pages/CoursesPage"
 import ContactPage from "./pages/ContactPage"
 // import AdmissionPage from "./pages/AdmissionPage
 import ChatbotComponent from "./components/Chatbot/ChatbotComponent"
-import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperinfoPopup"
-import { useState } from "react"
+
 
 
 const App = () => {
- const [showPopup, setShowPopup] = useState(true);
-  const handleClosePopup = () => {
-    setShowPopup(false);
-  };
+ 
   return (
    <>
-   <div>
-        {/* Your main application content */}
-        <DeveloperInfoPopup
-          show={showPopup}
-          onClose={handleClosePopup}
-          studentName="Aditya Adinath Kumbhar"
-          studentPhotoUrl="/images/aak.jpg" 
-          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
-        />
-      </div>
+  
    <Router>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
