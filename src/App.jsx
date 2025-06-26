0cd9559c7dd173df895979d844b6c3d7b5de2081
Aditya <adityak4347@gmail.com>
2025-06-27 05:05:40 +0530

    sixth commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
           show={showPopup}
           onClose={handleClosePopup}
           studentName="Aditya Adinath Kumbhar"
-          studentPhotoUrl="/images/aak.jpg" // Path to their photo
+          studentPhotoUrl="/images/cmps.jpg" // Path to their photo
           uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
         />
       </div>
